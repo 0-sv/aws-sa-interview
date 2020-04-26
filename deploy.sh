ddc51012209e9eaf9ef1d66ae2f6b5f55e07faae
Sebastiaan Verbeek <contact@svatwork.com>
2020-04-26 12:43:45 +0700

    Submitted to AWS for review

--- deploy.sh
+++ deploy.sh
@@ -10,13 +10,15 @@ DIR="file://$(pwd)"
 
 REGION="eu-west-1"
 
-if ! aws cloudformation validate-template --template-body=$DIR/$OLD; then
-    echo "There are some errors with your $OLD template." && \
+if ! aws cloudformation validate-template --template-body=$DIR/$OLD &>/dev/null; then
+    echo "There are some errors with your $OLD template. \
+        Please run aws cloudformation validate-template outside this script." && \
     exit 1;
 fi
 
-if ! aws cloudformation validate-template --template-body=$DIR/$NEW; then
-    echo "There are some errors with your $NEW template." && \
+if ! aws cloudformation validate-template --template-body=$DIR/$NEW &>/dev/null; then
+    echo "There are some errors with your $OLD template. \
+        Please run aws cloudformation validate-template outside this script." && \
     exit 1;
 fi
 
@@ -27,8 +29,8 @@ fi
 aws cloudformation create-stack \
         --stack-name=$STACKNAME \
         --template-body="$DIR/$OLD" \
-        --parameters="ParameterKey=CandidateName,ParameterValue=$CANDIDATENAME" 2>/dev/null
+        --parameters="ParameterKey=CandidateName,ParameterValue=$CANDIDATENAME" 2>/dev/null && exit 1;
 
 aws cloudformation update-stack --stack-name=$STACKNAME \
     --template-body="$DIR/$NEW" \
-    --parameters="ParameterKey=CandidateName,ParameterValue=$CANDIDATENAME"
+    --parameters="ParameterKey=CandidateName,ParameterValue=$CANDIDATENAME" 
